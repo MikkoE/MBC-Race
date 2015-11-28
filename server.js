cb23a173302c748da628cc412f15e589a0dbdfca
Mikko Eberhardt <mikko.eberhardt@haw-hamburg.de>
2015-11-28 23:15:34 +0100

    commit work on server for socket.io

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 var qr = require('qr-image');
 var fs = require('fs');
-var express = require('express');
-var app = express();
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.use('/static', express.static(__dirname + '/Desktop'));
+
 
 app.get('/', function(req, res) {
 
 
   fs.readFile('index.html',function (err, data){
+
+
+
       res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
       res.write(data);
       res.end();
   });
 });
+
+
 
 app.get('/qr/', function (req, res) {
   var code = qr.image(new Date().toString(), { type: 'svg' });
@@ -22,9 +28,21 @@ app.get('/qr/', function (req, res) {
 });
 
 
-var server = app.listen(3000, function () {
+var server = http.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
+});
+
+
+///////////// Sockets ///////////
+io.on('connection', function(socket){
+  console.log('connected');
+
+
+  socket.on('clicked', function(msg){
+    console.log('button clicked');
+  });
+
 });
